69a58c48cdb16f411d4e9391f0cb4a79e1b0d0d5
cedricbasuel <cedric.basuel@gmail.com>
2021-02-23 14:10:26 +0800

    [initial commit] added sentiment_analysis_via_api.py

---
+++ sentiment_analysis_via_api.py
@@ -0,0 +1,93 @@
+'''Script for performing sentiment analysis from trained model
+   or paid cloud APIs.
+
+Usage:
+    python sentiment_analysis_via_api.py config.py 
+
+Author:
+    Cedric Basuel
+
+Notes:
+    If using the Google API, set env variables in terminal first.
+    For example: `export GOOGLE_APPLICATION_CREDENTIALS="/Users/service_key.json"`
+'''
+
+import sys
+import yaml
+from google.cloud import language_v1
+import pandas as pd
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+
+
+def get_sentiment_score_using_google(text_list):
+    client = language_v1.LanguageServiceClient()
+
+    text_sentiment = []
+    text_score = []
+
+    for text in text_list:  
+        document = language_v1.Document(content=text,type_=language_v1.Document.Type.PLAIN_TEXT)
+        sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+        text_score.append(sentiment.score)
+
+        sentiment_ = 'positive' if sentiment.score > 0 else 'negative' # This is just me
+        text_sentiment.append(sentiment_)
+
+    return text_list, text_sentiment, text_score
+
+
+def get_sentiment_score_using_azure(text_list, credential, client):
+    credential=AzureKeyCredential(credential)
+    text_analytics_client=TextAnalyticsClient(endpoint=client, credential=credential)
+
+    text_sentiment = []
+    text_score = []
+
+    for text in text_list:
+        response = text_analytics_client.analyze_sentiment([text],language='en')
+
+        temp_sentiment = response[0].sentiment
+        # try KeyError temp_sentiment is 'mixed'
+        try:
+            temp_score = response[0].confidence_scores[temp_sentiment]
+        except KeyError:
+            temp_score = 0.5
+
+        text_sentiment.append(temp_sentiment)
+        text_score.append(temp_score)
+
+    return text_list, text_sentiment, text_score
+
+
+if __name__ == '__main__' :
+    CONFIG_FILE = sys.argv[1]
+
+    with open(CONFIG_FILE) as cfg:
+        config = yaml.safe_load(cfg)
+
+    tweets2019 = pd.read_csv(config['data']['tweets_2019'])
+    # tweets2020 = pd.read_csv(config['data']['tweets_2020'])
+
+    text_list_2019 = list(tweets2019['text'])
+    text_list_2019 = text_list_2019[0:10]
+    # text_list_2020 = list(tweets2020['text'])
+
+    # Use Google Natural Language API
+    text_list_, text_sentiment, text_score = get_sentiment_score_using_google(text_list_2019)
+
+    # Use Microsoft Text Analytics
+    # text_list_, text_sentiment, text_score = get_sentiment_score_using_azure(
+    #     text_list_2019,
+    #     credential=config['azure_key'],
+    #     client=config['azure_endpoint'])
+
+    # Save output
+    # df = pd.DataFrame({
+    #     'text':text_list_,
+    #     'sentiment':text_sentiment,
+    #     'score':text_score})
+
+    # df.to_csv(config['output']['google'])
+
+
